241a1b7258791b754a8637229874d4e57f761326
Queiroz <63520242+queirozime@users.noreply.github.com>
2022-05-17 20:16:53 -0300

    Update 19.cpp

--- 19.cpp
+++ 19.cpp
@@ -23,3 +23,24 @@ public:
         return head;
     }
 };
+
+//two pointer solution
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        for(int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+        
+        if(!fast) return head->next;
+        while(fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+        slow->next = slow->next->next;
+        return head;
+    }
+};
